1862c397aea239ac4451c0190e7d31d2edaf016a
Jonas Dally <jonas.dally@hotmail.de>
2024-01-05 16:42:50 +0100

    created responsive navigation bar, fix the size responsive for the iphone and improve the background performance

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,26 @@
+import {  useEffect, useState } from 'react';
 import GlobalStyle from '../styles';
 
 
 export default function App({ Component, pageProps }) {
+  const [isMobile, setMobile] = useState(false);
+
+// useEffect with innerWidth to detect mobile device for responsive design
+  useEffect(() => {
+  const updateMedia = () => {
+      setMobile(window.innerWidth < 915);
+    };
+    updateMedia();
+    window.addEventListener("resize", updateMedia);
+    return () => window.removeEventListener("resize", updateMedia);
+  }
+  );
+
+
     return ( 
     <>
     <GlobalStyle />
-
-    <Component {...pageProps} />
+    <Component {...pageProps} isMobile={isMobile}/>
     </>
     
     )
