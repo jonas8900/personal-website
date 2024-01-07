5d17761b8897c21cf78655be5906a4b6b2e695d1
Jonas Dally <jonas.dally@hotmail.de>
2024-01-07 13:25:28 +0100

    try to fix the background, the tests doesnt work, this needs to be fixed at the end of the project because of many other options.

--- pages/index.js
+++ pages/index.js
@@ -2,17 +2,28 @@
 import styled from 'styled-components';
 import Spline from '@splinetool/react-spline';
 import NavigationBar from '@/components/Navbar/navigationBar';
+import { useEffect, useRef, useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleNotch } from '@fortawesome/free-solid-svg-icons';
+import Image from 'next/image';
 
 export default function Page({isMobile}) {
+  //const [isloading, setLoading] = useState(false); // this needs to be fixed at the end of the project to be able to use the spline background
+
 
     return (
       <StyledMain>
-            <StyledSplineBackgroundWrapper>
-        {/*  <Spline scene="https://prod.spline.design/gSfN3MXvPkAzNfml/scene.splinecode" />*/}
-        <script type="module" src="https://unpkg.com/@splinetool/viewer@1.0.17/build/spline-viewer.js" async/>
-<spline-viewer url="https://prod.spline.design/gSfN3MXvPkAzNfml/scene.splinecode"></spline-viewer>
+        <StyledBackgroundImage src="/images/Backgroundimage.png" alt="3d Backgroundpicture with white and blue colored artefact" width={1920} height={1080} unoptimized/>
+        {/* this needs to be fixed at the end of the project to be able to use the spline background */}
+      {/* {isloading ? (
+        <StyledLoadingIcon icon={faCircleNotch} spin />
 
-        </StyledSplineBackgroundWrapper>
+      ):(
+      <>
+            <StyledSplineBackgroundWrapper>
+
+      <Spline onLoad={() => setLoading(false)} scene="https://draft.spline.design/BuZzbLISnCnN8Cev/scene.splinecode" /> 
+        </StyledSplineBackgroundWrapper> */}
         <StyledMainSection>
             <NavigationBar isMobile={isMobile}/>
       
@@ -20,6 +31,8 @@ export default function Page({isMobile}) {
         <StyledDivForSplineMobilePhone>
         <Spline scene="https://draft.spline.design/5viGrrJfn78PDoLV/scene.splinecode" />
         </StyledDivForSplineMobilePhone>
+
+  
       </StyledMain>
     );
 
@@ -27,18 +40,11 @@ export default function Page({isMobile}) {
 
   }
   
+
+
   const StyledMain = styled.main`
-
-`;
-
-
-  const StyledSplineBackgroundWrapper = styled.div`
-  position: fixed;
-  width: 100%;
-  height: 100%;
-  top: 0;
-
-
+width: 100%;
+height: 100%;
 `;
 
 
@@ -52,6 +58,19 @@ border-bottom: 1px solid black;
 background-color: rgba(255,255,255,0.5);
 box-shadow: 0 0 10px rgba(0,0,0,0.5);
 `;
+
+const StyledBackgroundImage = styled(Image)` // temporary solution for the background image to match the design
+position: fixed;
+`;
+
+//wrapper for spline background
+  const StyledSplineBackgroundWrapper = styled.div` // this needs to be fixed at the end of the project to be able to use the spline background
+  position: fixed;
+  width: 100vw;
+  height: 100vh;
+  top: 0;
+`;
+
 
 const StyledDivForSplineMobilePhone = styled.div`
   position: absolute;
@@ -67,4 +86,14 @@ const StyledDivForSplineMobilePhone = styled.div`
 @media (max-height: 500px ) {
   display: none;
 }
+`;
+
+
+//icons
+
+const StyledLoadingIcon = styled(FontAwesomeIcon)` // this needs to be fixed at the end of the project to be able to use the spline background
+  margin: auto;
+  width: 10rem;
+  height: 10rem;
+  color: var(--primary-blue);
 `;
